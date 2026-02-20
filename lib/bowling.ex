6202f5a8642632466fcb49ea0372547c17ec221b
marianoC31 <mariano05055@gmail.com>
2026-02-19 18:38:53 -0600

    Versión terminada, maneja los strikes seguidos accediendo al tiro siguiente ignorandi nils, usé pattern matching, maneja el caso final de 3strikes en los tiros especiales

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,15 +1,18 @@
 defmodule Bowling do
   def score([]), do: 0
   #Caso BASE
-  """
+
   def score([[10,10,extra]|others]) do
       10 + 10 + extra + score(others)
   end
-  """
   def score([[10|_]|others]) do
     [f,s|_] = hd(others)
+    if s == nil do
+      [_,[extra,_|_]|_] = others
+      10 + f + extra + score(others)
+    else
       10 + f + s + score(others)
-
+    end
 
   end
 
