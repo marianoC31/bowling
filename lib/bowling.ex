3f3c0b06ee3456e81f5e713263e925180e5dc2d2
marianoC31 <mariano05055@gmail.com>
2026-02-19 18:39:21 -0600

    Comentado

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -5,6 +5,7 @@ defmodule Bowling do
   def score([[10,10,extra]|others]) do
       10 + 10 + extra + score(others)
   end
+  #Caso 10, 10 final decimo
   def score([[10|_]|others]) do
     [f,s|_] = hd(others)
     if s == nil do
@@ -13,11 +14,11 @@ defmodule Bowling do
     else
       10 + f + s + score(others)
     end
-
+  #Caso strikes seguidos
   end
 
   def score([[f,s|_]|others]) when f + s == 10, do: f+ s +  hd(hd(others)) + score(others)
 
   def score([[f,s|_]|others]), do:  f + s + score(others)
-
+  #Casos normales, spares y sin bonuses
 end
