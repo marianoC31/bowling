32e497615155756d48b1d56f5baad70d4967d2ae
marianoC31 <mariano05055@gmail.com>
2026-02-19 18:33:43 -0600

    Segunda versión, maneja los casos donde hay spares, no maneja nils

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -14,8 +14,9 @@ defmodule Bowling do
     end
 
   end
-  def score([[f,s|_]|others]) when f + s == 10, do: f+ s +  hd(hd(others)) + score(others)
   """
+  def score([[f,s|_]|others]) when f + s == 10, do: f+ s +  hd(hd(others)) + score(others)
+
   def score([[f,s|_]|others]), do:  f + s + score(others)
 
 end
